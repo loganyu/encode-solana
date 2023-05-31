b3ed3eb2801380a954723ef3bd248be38489ce20
Logan Yu <yu.logan@gmail.com>
2023-05-31 01:07:47 +0000

    homework 6

--- homeworks_rust/homeworks/homework6/collections/hashmap2.rs
+++ homeworks_rust/homeworks/homework6/collections/hashmap2.rs
@@ -12,7 +12,6 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -38,6 +37,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        if !basket.contains_key(&fruit) {
+            // Add the fruit to the basket
+            basket.insert(fruit, 1);
+        }
     }
 }
 
--- homeworks_rust/homeworks/homework6/enums/enums2.rs
+++ homeworks_rust/homeworks/homework6/enums/enums2.rs
@@ -1,11 +1,13 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Move { x: i32, y: i32 },                // Variant with named fields
+    Echo(String),                           // Variant with a single field
+    ChangeColor(u8, u8, u8),                 // Variant with multiple fields
+    Quit,                                   // Unit variant (no fields)
 }
 
 impl Message {
